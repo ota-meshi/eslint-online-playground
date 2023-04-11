989e9fa8a6b994cbbb12fd50e5f19eabbee7e9b9
yosuke ota <otameshiyo23@gmail.com>
2023-04-11 20:01:35 +0900

    update switch design

--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -11,20 +11,19 @@ export const themeValue = ref<"dark" | "light">(
 
 <script setup lang="ts">
 function switchTheme() {
-  document.documentElement.classList.toggle("dark");
-  const theme = document.documentElement.classList.contains("dark")
-    ? "dark"
-    : "light";
-  localStorage.setItem("theme", theme);
-  themeValue.value = theme;
+  themeValue.value = themeValue.value === "dark" ? "light" : "dark";
+  document.documentElement.classList.toggle(
+    "dark",
+    themeValue.value === "dark"
+  );
+  localStorage.setItem("theme", themeValue.value);
 }
 </script>
 
 <template>
   <button class="ep-theme-switch" @click="switchTheme">
-    <div class="ep-theme-switch__icon">
-      <div class="ep-theme-switch__sun">☀</div>
-      <div class="ep-theme-switch__moon">🌙</div>
+    <div class="ep-theme-switch__icon-button">
+      <div class="ep-theme-switch__icon"></div>
     </div>
   </button>
 </template>
@@ -38,11 +37,12 @@ function switchTheme() {
   height: 22px;
   border: 1px solid var(--ep-border-color);
   background-color: var(--ep-background-color);
-  transition: border-color 0.25s;
   padding: 1px;
+
+  --ep-theme-switch-animation-duration: 300ms;
 }
 
-.ep-theme-switch__icon {
+.ep-theme-switch__icon-button {
   box-sizing: border-box;
   height: 18px;
   width: 18px;
@@ -53,27 +53,62 @@ function switchTheme() {
   border: 1px solid var(--ep-border-color);
   position: absolute;
   left: 2px;
-  transition: left 0.25s;
+  transition: left var(--ep-theme-switch-animation-duration);
 }
-
-.ep-theme-switch__sun {
-  color: orange;
-  display: block;
-}
-
-.ep-theme-switch__moon {
-  color: orange;
-  display: none;
-}
-
-.dark .ep-theme-switch__icon {
+.dark .ep-theme-switch__icon-button {
   left: 18px;
 }
 
-.dark .ep-theme-switch__sun {
-  display: none;
+.ep-theme-switch__icon {
+  display: flex;
+  height: 12px;
+  width: 12px;
+  box-sizing: border-box;
 }
-.dark .ep-theme-switch__moon {
+.ep-theme-switch__icon:before {
+  content: "";
   display: block;
+  margin: auto;
+  box-sizing: border-box;
+  border-radius: 50%;
+  background-color: var(--ep-background-color);
+  transition: height var(--ep-theme-switch-animation-duration),
+    width var(--ep-theme-switch-animation-duration),
+    box-shadow var(--ep-theme-switch-animation-duration);
+
+  /* sun */
+  height: 8px;
+  width: 8px;
+  box-shadow: -12px -12px 0 0 orange inset;
+}
+.ep-theme-switch__icon:after {
+  content: "";
+  display: block;
+  box-sizing: border-box;
+  border-radius: 50%;
+  height: 2px;
+  width: 2px;
+  position: absolute;
+  left: calc(50% - 1px);
+  top: calc(50% - 1px);
+  transition: box-shadow var(--ep-theme-switch-animation-duration),
+    opacity var(--ep-theme-switch-animation-duration);
+
+  /* sun */
+  opacity: 1;
+  box-shadow: 0 -5.5px 0 0 orange, -4px -4px 0 0 orange, -5.5px 0 0 0 orange,
+    -4px 4px 0 0 orange, 0 5.5px 0 0 orange, 4px 4px 0 0 orange,
+    5.5px 0 0 0 orange, 4px -4px 0 0 orange;
+}
+
+.dark .ep-theme-switch__icon:before {
+  /* moon */
+  height: 12px;
+  width: 12px;
+  box-shadow: -3px -2px 0 0 yellow inset;
+}
+.dark .ep-theme-switch__icon:after {
+  opacity: 0;
+  box-shadow: none;
 }
 </style>
