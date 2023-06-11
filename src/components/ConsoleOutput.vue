68fb2a8295dddac41297b0981be6cbddcb7fae49
yosuke ota <otameshiyo23@gmail.com>
2023-06-11 13:57:14 +0900

    use xterm

--- src/components/ConsoleOutput.vue
+++ src/components/ConsoleOutput.vue
@@ -1,70 +1,75 @@
 <script setup lang="ts">
-import { nextTick, ref } from "vue";
-import ansiRegex from "ansi-regex";
-
-const CHA = "\u001b[1G";
-const EL = "\u001b[K";
-
-let nextEraseInLine = false;
+import { nextTick, ref, watch } from "vue";
+import "xterm/css/xterm.css";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
 
 const element = ref<HTMLDivElement>();
-const consoleText = ref("");
+const termBuffer: (() => void)[] = [];
+let term: Terminal | null = null;
+
+watch(element, (el) => {
+  if (!el) return;
+
+  term = new Terminal({
+    fontSize: 12,
+  });
+
+  const fitAddon = new FitAddon();
+  term.loadAddon(fitAddon);
+
+  term.open(el);
+  fitAddon.fit();
+  for (const fn of termBuffer) {
+    fn();
+  }
+
+  const resizeObserver = new ResizeObserver(() => {
+    if (el.clientWidth) fitAddon.fit();
+  });
+  resizeObserver.observe(el);
+});
 
 function appendLine(string: string) {
-  const currContent = (consoleText.value = consoleText.value.trimEnd());
-
-  append(`${(currContent ? "\n" : "") + string}\n`);
+  if (term) {
+    term.writeln(string);
+  } else {
+    termBuffer.push(() => appendLine(string));
+  }
 }
 
 function append(string: string) {
-  const ansiRe = ansiRegex();
-  let start = 0;
-
-  for (const match of string.matchAll(ansiRe)) {
-    if (match[0] === CHA || match[0] === EL) {
-      nextEraseInLine = true;
-    }
-
-    append(string.slice(start, match.index));
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- ok
-    start = match.index! + match[0].length;
+  if (term) {
+    term.write(string);
+  } else {
+    termBuffer.push(() => append(string));
   }
-
-  append(string.slice(start));
 
   void nextTick().then(() => {
     if (element.value) {
       element.value.scrollTop = element.value.scrollHeight;
     }
   });
-
-  function append(s: string) {
-    const str = s.replace(/\r/g, "");
-    if (!str) return;
-
-    if (nextEraseInLine) {
-      eraseInLine();
-
-      nextEraseInLine = false;
-    }
-
-    consoleText.value += str;
-  }
-
-  function eraseInLine() {
-    const lastLinefeed = consoleText.value.lastIndexOf("\n");
-    if (lastLinefeed > -1)
-      consoleText.value = consoleText.value.slice(0, lastLinefeed + 1);
-  }
 }
 
 function clear() {
-  consoleText.value = "";
+  term?.clear();
+  termBuffer.length = 0;
 }
 
 defineExpose({ appendLine, append, clear });
 </script>
 
 <template>
-  <div ref="element" class="ep-console ep-output-panel">{{ consoleText }}</div>
+  <div class="ep-console ep-output-panel">
+    <div ref="element" class="ep-xterm-wrapper"></div>
+  </div>
 </template>
+
+<style scoped>
+.ep-xterm-wrapper {
+  position: relative;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
