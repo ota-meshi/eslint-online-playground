e15d94230c4a9ae286087be2ea1ea9b065757bf0
Yosuke Ota <otameshiyo23@gmail.com>
2023-10-08 00:14:22 +0000

    fix

--- src/monaco-editor/monaco-loader.ts
+++ src/monaco-editor/monaco-loader.ts
@@ -130,7 +130,7 @@ async function appendMonacoEditorScript(): Promise<HTMLScriptElement> {
   const script = document.createElement("script");
 
   return new Promise((resolve) => {
-    script.src = `https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/${monacoVersion}/min/vs/loader.min.js`;
+    script.src = `https://unpkg.com/monaco-editor/${monacoVersion}/min/vs/loader.min.js`;
     script.onload = () => {
       script.onload = null;
 
