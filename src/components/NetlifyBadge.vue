9adf771d1c6bedd63a2f2baf3d6fc3df2ac73849
Yosuke Ota <otameshiyo23@gmail.com>
2023-12-21 16:14:34 +0000

    fix

--- src/components/NetlifyBadge.vue
+++ src/components/NetlifyBadge.vue
@@ -8,10 +8,10 @@
     class="netlify-light-badge"
     v-bind="$attrs"
   >
-    <g clip-path="url(#a)">
+    <g clip-path="url(#a_light)">
       <rect width="114" height="50" rx="4" fill="#05BDBA" />
       <path fill="#fff" d="M0 0h114v51H0z" />
-      <g clip-path="url(#b)">
+      <g clip-path="url(#b_light)">
         <path
           d="M44.445 40.302v-7.695l.153-.16h1.853l.154.16v7.695l-.154.16h-1.853l-.153-.16ZM44.445 17.855v-7.696l.153-.159h1.853l.154.16v7.695l-.154.16h-1.853l-.153-.16ZM38.068 34.892h-.254l-1.275-1.323v-.264l2.388-2.482 1.349.002.18.185v1.4l-2.388 2.482ZM36.54 17.227v-.267l1.274-1.32h.254l2.388 2.479v1.398l-.18.19h-1.349l-2.388-2.48ZM28.154 24.107h10.5l.153.16v1.926l-.153.16h-10.5l-.154-.16v-1.927l.154-.16ZM99.844 24.11l.154.159v1.924l-.154.16H89.19l-.153-.16.77-1.924.154-.16h9.883Z"
           fill="#05BDBA"
@@ -37,10 +37,10 @@
       stroke-width="2"
     />
     <defs>
-      <clipPath id="a">
+      <clipPath id="a_light">
         <rect width="114" height="50" rx="4" fill="#fff" />
       </clipPath>
-      <clipPath id="b"><path fill="#fff" d="M28 10h72v30.462H28z" /></clipPath>
+      <clipPath id="b_light"><path fill="#fff" d="M28 10h72v30.462H28z" /></clipPath>
     </defs>
   </svg>
   <svg
@@ -52,9 +52,9 @@
     class="netlify-dark-badge"
     v-bind="$attrs"
   >
-    <g clip-path="url(#a)">
+    <g clip-path="url(#a_dark)">
       <path fill="#0C2A2A" d="M0 0h114v51H0z" />
-      <g clip-path="url(#b)">
+      <g clip-path="url(#b_dark)">
         <path
           d="M44.445 40.302v-7.695l.153-.16h1.853l.154.16v7.695l-.154.16h-1.853l-.153-.16ZM44.445 17.855v-7.696l.153-.159h1.853l.154.16v7.695l-.154.16h-1.853l-.153-.16ZM38.068 34.892h-.254l-1.275-1.323v-.264l2.388-2.482 1.349.002.18.185v1.4l-2.388 2.482ZM36.54 17.227v-.267l1.274-1.32h.254l2.388 2.479v1.398l-.18.19h-1.349l-2.388-2.48ZM28.154 24.107h10.5l.153.16v1.926l-.153.16h-10.5l-.154-.16v-1.927l.154-.16ZM99.844 24.11l.154.159v1.924l-.154.16H89.19l-.153-.16.77-1.924.154-.16h9.883Z"
           fill="#32E6E2"
@@ -80,10 +80,10 @@
       stroke-width="2"
     />
     <defs>
-      <clipPath id="a">
+      <clipPath id="a_dark">
         <rect width="114" height="50" rx="4" fill="#fff" />
       </clipPath>
-      <clipPath id="b"><path fill="#fff" d="M28 10h72v30.462H28z" /></clipPath>
+      <clipPath id="b_dark"><path fill="#fff" d="M28 10h72v30.462H28z" /></clipPath>
     </defs>
   </svg>
 </template>
