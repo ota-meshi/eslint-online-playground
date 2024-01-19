24f1a4f9154baef664d13a20d02ef21541cf2115
ota-meshi <otameshiyo23@gmail.com>
2024-01-20 00:59:19 +0900

    change loading animation

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -31,56 +31,32 @@ function close() {
   overflow: visible;
 }
 
-/* Copied from https://css-loaders.com/shapes/ */
+/* Copied from https://css-loaders.com/spinner/ */
 .loader {
-  width: 60px;
-  aspect-ratio: 1.154;
-  color: #25b09b;
+  width: 50px;
+  aspect-ratio: 1;
   display: grid;
-  animation: l38-0 6s infinite linear;
+  border: 4px solid #0000;
+  border-radius: 50%;
+  border-right-color: #25b09b;
+  animation: l15 1s infinite linear;
 }
-.loader:before,
-.loader:after {
+.loader::before,
+.loader::after {
   content: "";
   grid-area: 1/1;
+  margin: 2px;
+  border: inherit;
+  border-radius: 50%;
+  animation: l15 2s infinite;
 }
-.loader:before {
-  clip-path: polygon(25% 0, 75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%);
-  background: conic-gradient(from -30deg, #0000 60deg, currentColor 0);
+.loader::after {
+  margin: 8px;
+  animation-duration: 3s;
 }
-.loader:after {
-  background: conic-gradient(from -30deg, currentColor 60deg, #0000 0);
-  animation: l38 0.5s infinite alternate;
-}
-@keyframes l38-0 {
-  0%,
-  16.66% {
-    transform: rotate(0deg);
-  }
-  16.67%,
-  33.33% {
-    transform: rotate(60deg);
-  }
-  33.34%,
-  50% {
-    transform: rotate(120deg);
-  }
-  50.1%,
-  66.66% {
-    transform: rotate(180deg);
-  }
-  66.67%,
-  83.33% {
-    transform: rotate(240deg);
-  }
-  83.34%,
+@keyframes l15 {
   100% {
-    transform: rotate(300deg);
-  }
-}
-@keyframes l38 {
-  100% {
-    transform: translateY(-20px);
+    transform: rotate(1turn);
   }
 }
 </style>
