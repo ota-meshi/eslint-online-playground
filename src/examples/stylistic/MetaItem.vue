601e683a42f81335159abaefbbd174791ade81eb
ota-meshi <otameshiyo23@gmail.com>
2024-01-21 02:13:22 +0900

    add stylistic example

---
+++ src/examples/stylistic/MetaItem.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+export const name = "ESLintStylistic";
+</script>
+<template>
+  <div>
+    Example to try
+    <a href="https://eslint.style/" target="_blank">ESLint Stylistic</a>.
+  </div>
+</template>
---
+++ src/examples/stylistic/package.json.ts
@@ -0,0 +1,11 @@
+export default {
+  type: "module",
+  devDependencies: {
+    eslint: "latest",
+    "@stylistic/eslint-plugin": "latest",
+    "@stylistic/eslint-plugin-js": "latest",
+    "@stylistic/eslint-plugin-ts": "latest",
+    "@stylistic/eslint-plugin-jsx": "latest",
+    "@stylistic/eslint-plugin-plus": "latest",
+  },
+};
