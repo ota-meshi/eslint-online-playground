438e5b99db2e7972806410c7a86f5b0bbfb3077c
Yosuke Ota <otameshiyo23@gmail.com>
2024-01-21 02:22:32 +0900

    add stylistic example (#53)

---
+++ src/examples/stylistic/MetaItem.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+export const name = "ESLintStylistic";
+export default {};
+</script>
+<template>
+  <div>
+    Example to try
+    <a href="https://eslint.style/" target="_blank">ESLint Stylistic</a>.
+  </div>
+</template>
