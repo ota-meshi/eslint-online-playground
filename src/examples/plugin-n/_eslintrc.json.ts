c1c76c60bb7e2d56176d5eb98409848661572a4b
yosuke ota <otameshiyo23@gmail.com>
2024-05-11 08:12:12 +0900

    Migrate plugin-n example to flat config

--- src/examples/plugin-n/_eslintrc.json.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ["eslint:recommended", "plugin:n/recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-};
--- src/examples/plugin-n/package.json.ts
+++ src/examples/plugin-n/package.json.ts
@@ -1,4 +1,5 @@
 export default {
+  type: "module",
   engines: {
     node: "^14.17.0 || >=16.0.0",
   },
