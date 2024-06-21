396bac91ae49e30239715776d49070b8439f54ff
yosuke ota <otameshiyo23@gmail.com>
2024-06-21 18:05:09 +0900

    feat: migrate some plugins to flat config

--- src/plugins/plugins/regexp/plugin.ts
+++ src/plugins/plugins/regexp/plugin.ts
@@ -1,4 +1,4 @@
-import type { ESLintLegacyConfig } from "../..";
+import type { ESLintConfig, ESLintLegacyConfig } from "../..";
 
 export const name = "eslint-plugin-regexp";
 export const description =
@@ -7,4 +7,19 @@ export const repo = "https://github.com/ota-meshi/eslint-plugin-regexp";
 export const devDependencies = { [name]: "latest" };
 export const eslintLegacyConfig: ESLintLegacyConfig = {
   extends: ["plugin:regexp/recommended"],
+};
+export const eslintConfig: ESLintConfig<"regexp"> = {
+  *imports(helper) {
+    if (helper.type === "module") {
+      yield helper.i(`import regexp from 'eslint-plugin-regexp'`);
+    } else {
+      yield helper.require({
+        local: "regexp",
+        source: "eslint-plugin-regexp",
+      });
+    }
+  },
+  *expression(names, helper) {
+    yield helper.x(`${names.regexp}.configs['flat/recommended']`);
+  },
 };
