444838496a91502ce8d7fc5efc034e4529b1d6cb
yosuke ota <otameshiyo23@gmail.com>
2024-06-21 20:48:08 +0900

    update plugin installer logic

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -49,27 +49,40 @@ export async function loadPlugins(): Promise<Record<string, Plugin>> {
   }
   allPlugins = {};
 
-  const list = await Promise.all([
-    ...Object.entries(import.meta.glob("./plugins/**/*.ts")).map(
-      async ([fileName, content]) => {
-        const plugin = { ...((await content()) as Plugin) };
-        if (!plugin.name) {
-          plugin.name = convertToName(fileName);
-        }
-        if (!plugin.hasInstalled) {
-          plugin.hasInstalled = function hasInstalled(
-            packageJson: any,
-          ): boolean {
-            return (
-              packageJson.devDependencies?.[plugin.name] != null ||
-              packageJson.dependencies?.[plugin.name] != null
-            );
-          };
-        }
-        return plugin;
-      },
-    ),
-  ]);
+  const list = (
+    await Promise.all([
+      ...Object.entries(import.meta.glob("./plugins/**/*.ts")).map(
+        async ([fileName, content]) => {
+          const plugin = { ...((await content()) as Plugin) };
+          if (!plugin.name) {
+            plugin.name = convertToName(fileName);
+          }
+          if (!plugin.hasInstalled) {
+            plugin.hasInstalled = function hasInstalled(
+              packageJson: any,
+            ): boolean {
+              return (
+                packageJson.devDependencies?.[plugin.name] != null ||
+                packageJson.dependencies?.[plugin.name] != null
+              );
+            };
+          }
+          return plugin;
+        },
+      ),
+    ])
+  ).filter((plugin) => {
+    if (
+      !plugin.devDependencies ||
+      Object.keys(plugin.devDependencies).length === 0
+    )
+      // If the additional dependencies are not present, it is not a plugin installer.
+      return false;
+    if (!plugin.eslintConfig && !plugin.eslintLegacyConfig)
+      // If the additional config is not present, it is not a plugin installer.
+      return false;
+    return true;
+  });
   for (const plugin of list.sort(({ name: a }, { name: b }) =>
     customCompare(a, b),
   )) {
--- src/plugins/plugins/cds/plugin.ts
+++ src/plugins/plugins/cds/plugin.ts
@@ -3,7 +3,6 @@ import type { ESLintLegacyConfig } from "../..";
 export const name = "@sap/eslint-plugin-cds";
 export const description =
   "SAP Cloud Application Programming Model (CAP) model and environment linting rules";
-export const depencencies = { "@sap/cds": "^7.0.0" };
 export const devDependencies = { [name]: "latest" };
 export const eslintLegacyConfig: ESLintLegacyConfig = {
   extends: ["plugin:@sap/cds/recommended"],
