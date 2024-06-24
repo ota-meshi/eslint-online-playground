3aa7f0dfa73b10592024ca4b5d46cdcc9aad3bd8
Daniel O'Grady <daniel.o-grady@sap.com>
2024-06-24 15:02:38 +0200

    Update plugin for cds

--- src/plugins/plugins/cds/plugin.ts
+++ src/plugins/plugins/cds/plugin.ts
@@ -1,9 +1,21 @@
-import type { ESLintLegacyConfig } from "../..";
+import type { ESLintConfig, ESLintLegacyConfig } from "../..";
 
 export const name = "@sap/eslint-plugin-cds";
-export const description =
-  "SAP Cloud Application Programming Model (CAP) model and environment linting rules";
+export const description = "SAP Cloud Application Programming Model (CAP) model and environment linting rules";
 export const devDependencies = { [name]: "latest" };
 export const eslintLegacyConfig: ESLintLegacyConfig = {
-  extends: ["plugin:@sap/cds/recommended"],
+  extends: ["plugin:@sap/cds/recommended-legacy"],
+};
+export const eslintConfig: ESLintConfig<"cds"> = {
+  *imports({type, i, require}) {
+    yield type === "module"
+      ? i(`import cds from "${name}"`)
+      : require({
+        local: "cds",
+        source: name,
+      });
+  },
+  *expression(names, helper) {
+    yield helper.x(`${names.cds}.configs.recommended`);
+  },
 };
