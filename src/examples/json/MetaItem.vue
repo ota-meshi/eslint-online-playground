3430995898bf973221cd29a974d423fc5505371d
yosuke ota <otameshiyo23@gmail.com>
2024-11-07 19:09:50 +0900

    Add eslint/json example

---
+++ src/examples/json/MetaItem.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export const name = "@eslint/json";
+</script>
+<script setup lang="ts">
+const pluginName = name;
+</script>
+<template>
+  <div>
+    Example to try
+    <a href="https://github.com/eslint/json" target="_blank">{{ pluginName }}</a
+    >.
+  </div>
+</template>
---
+++ src/examples/json/package.json.ts
@@ -0,0 +1,7 @@
+export default {
+  type: "module",
+  devDependencies: {
+    "@eslint/json": "latest",
+    eslint: "latest",
+  },
+};
