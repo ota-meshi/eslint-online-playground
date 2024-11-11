1378577bea3209ba1c67de97620792e2c3431ad5
Yosuke Ota <otameshiyo23@gmail.com>
2024-11-11 09:56:35 +0900

    update json examples (#104)

---
+++ src/examples/plugin-jsonc_with_json/MetaItem.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+const jsonc = "eslint-plugin-jsonc";
+const json = "@eslint/json";
+export const name = `${jsonc} with ${json}`;
+</script>
+<script setup lang="ts">
+const pluginName = jsonc;
+</script>
+<template>
+  <div>
+    Example of using
+    <a
+      href="https://ota-meshi.github.io/eslint-plugin-jsonc/"
+      target="_blank"
+      >{{ pluginName }}</a
+    >
+    and
+    <a href="https://github.com/eslint/json" target="_blank">{{ json }}</a>
+    together.
+  </div>
+</template>
