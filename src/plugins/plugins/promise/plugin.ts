ad63b02dd5b2a47b16534e9dd91f5719df30bfd1
yosuke ota <otameshiyo23@gmail.com>
2024-12-04 08:13:40 +0900

    feat: add support form plugin-promise flat config

--- src/plugins/plugins/promise/plugin.ts
+++ src/plugins/plugins/promise/plugin.ts
@@ -1,4 +1,4 @@
-import type { ESLintLegacyConfig } from "../..";
+import type { ESLintConfig, ESLintLegacyConfig } from "../..";
 
 export const name = "eslint-plugin-promise";
 export const description = "Enforce best practices for JavaScript promises";
@@ -6,4 +6,19 @@ export const repo = "https://github.com/eslint-community/eslint-plugin-promise";
 export const devDependencies = { [name]: "latest" };
 export const eslintLegacyConfig: ESLintLegacyConfig = {
   extends: ["plugin:promise/recommended"],
+};
+export const eslintConfig: ESLintConfig<"pluginPromise"> = {
+  *imports(helper) {
+    if (helper.type === "module") {
+      yield helper.i(`import pluginPromise from '${name}'`);
+    } else {
+      yield helper.require({
+        local: "pluginPromise",
+        source: name,
+      });
+    }
+  },
+  *expression(names, helper) {
+    yield helper.x(`${names.pluginPromise}.configs['flat/recommended']`);
+  },
 };
