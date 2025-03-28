07fa28cd4cd8e62860ce408b736a8701ba41bca9
yosuke ota <otameshiyo23@gmail.com>
2025-03-28 09:30:46 +0900

    add `@intlify/eslint-plugin-svelte` example

---
+++ src/examples/intlify-plugin-svelte/MetaItem.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export const name = "@intlify/eslint-plugin-svelte";
+export default {};
+</script>
+<template>
+  <div>
+    Example to try
+    <a href="https://github.com/intlify/eslint-plugin-svelte" target="_blank"
+      >@intlify/eslint-plugin-svelte</a
+    >.
+  </div>
+</template>
---
+++ src/examples/intlify-plugin-svelte/meta.ts
@@ -0,0 +1,3 @@
+export { default as description, name } from "./MetaItem.vue";
+export const githubResources =
+  "https://github.com/intlify/eslint-plugin-svelte/tree/main/playground";
