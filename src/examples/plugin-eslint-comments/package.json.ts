7ae27dcabcc366e86bb4baf896de0a9268d0585d
yosuke ota <otameshiyo23@gmail.com>
2025-04-04 13:51:19 +0900

    add eslint-plugin-eslint-comments

---
+++ src/examples/plugin-eslint-comments/package.json.ts
@@ -0,0 +1,7 @@
+export default {
+  type: "module",
+  devDependencies: {
+    eslint: "latest",
+    "@eslint-community/eslint-plugin-eslint-comments": "latest",
+  },
+};
---
+++ src/plugins/plugins/eslint-comments/plugin.ts
@@ -0,0 +1,28 @@
+import type { ESLintConfig, ESLintLegacyConfig, PluginMeta } from "../..";
+
+export const name = "@eslint-community/eslint-plugin-eslint-comments";
+export const meta: PluginMeta = {
+  description: "Additional ESLint rules for directive comments of ESLint.",
+  repo: "https://github.com/eslint-community/eslint-plugin-eslint-comments",
+};
+export const devDependencies = { [name]: "latest" };
+export const eslintLegacyConfig: ESLintLegacyConfig = {
+  extends: ["plugin:@eslint-community/eslint-comments/recommended"],
+};
+export const eslintConfig: ESLintConfig<"comments"> = {
+  *imports(helper) {
+    if (helper.type === "module") {
+      yield helper.i(
+        `import comments from "@eslint-community/eslint-plugin-eslint-comments/configs"`,
+      );
+    } else {
+      yield helper.require({
+        local: "comments",
+        source: "@eslint-community/eslint-plugin-eslint-comments/configs",
+      });
+    }
+  },
+  *expression(names, helper) {
+    yield helper.x(`${names.comments}.recommended`);
+  },
+};
