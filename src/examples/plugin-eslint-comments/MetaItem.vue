1827b2250f37a6c5ec5fb31ece92de4d1f6aec4b
Yosuke Ota <otameshiyo23@gmail.com>
2025-04-04 13:52:38 +0900

    add eslint-plugin-eslint-comments (#124)

---
+++ src/examples/plugin-eslint-comments/MetaItem.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export const name = "eslint-plugin-eslint-comments";
+</script>
+<script setup lang="ts">
+const pluginName = name;
+</script>
+<template>
+  <div>
+    Example to try
+    <a
+      :href="'https://github.com/eslint-community/' + pluginName"
+      target="_blank"
+      >{{ pluginName }}</a
+    >.
+  </div>
+</template>
