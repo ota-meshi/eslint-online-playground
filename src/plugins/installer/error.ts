871ee96bbfe99507e9015fcac4bd84377832066c
yosuke ota <otameshiyo23@gmail.com>
2025-06-30 16:38:34 +0900

    chore: update esrap

--- src/plugins/installer/error.ts
+++ src/plugins/installer/error.ts
@@ -1,6 +1,6 @@
 export function alertAndLog(message: string): void {
   // eslint-disable-next-line no-alert -- OK
-  alert(message);
+  if (typeof alert === "function") alert(message);
   // eslint-disable-next-line no-console -- OK
   console.log(message);
 }
