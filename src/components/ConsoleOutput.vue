56f4aa067b04b913459b9337f79ffbf11780306a
yosuke ota <otameshiyo23@gmail.com>
2025-11-25 09:44:29 +0900

    Migrate xterm packages

--- src/components/ConsoleOutput.vue
+++ src/components/ConsoleOutput.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { nextTick, ref, watch } from "vue";
-import "xterm/css/xterm.css";
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
+import "@xterm/xterm/css/xterm.css";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
 import { debounce } from "../utils/debounce";
 
 const element = ref<HTMLDivElement>();
