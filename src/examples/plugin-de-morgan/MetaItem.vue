880af34332dab1e0ba76fadbf601c42846898078
Azat S. <to@azat.io>
2025-12-07 14:56:56 +0300

    feat: add eslint-plugin-de-morgan support

---
+++ src/examples/plugin-de-morgan/MetaItem.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export const name = "eslint-plugin-de-morgan";
+</script>
+<script setup lang="ts">
+const pluginName = name;
+</script>
+<template>
+  <div>
+    Example to try
+    <a
+      href="https://github.com/azat-io/eslint-plugin-de-morgan"
+      target="_blank"
+      >{{ pluginName }}</a
+    >.
+  </div>
+</template>
